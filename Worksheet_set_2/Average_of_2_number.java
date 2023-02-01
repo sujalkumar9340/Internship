cbebd89242b8b4a77dae056be434c25e8d31b501
sujalkumar9340 <110787167+sujalkumar9340@users.noreply.github.com>
2023-02-01 20:50:31 +0530

    Create Average_of_2_number.java
    Average of 2 number in java

---
+++ Worksheet_set_2/Average_of_2_number.java
@@ -0,0 +1,18 @@
+package Araaysprts;
+// Average of 2 number in java
+import java.util.Scanner;
+
+public class Average_of_2Num {
+public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter 2 number for average");
+	int number1=sc.nextInt();
+	int number2=sc.nextInt();
+	
+    System.out.println("average of 2 number = "+(number1+number2)/2);
+
+
+
+
+}
+}
